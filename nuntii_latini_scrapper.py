5331756fb248311e749008c0b0d423b031b1f427
Caio Begotti <caio1982@gmail.com>
2012-03-03 08:43:33 -0300

    export the news to individual XML files

--- nuntii_latini_scrapper.py
+++ nuntii_latini_scrapper.py
@@ -6,6 +6,8 @@
 
 import re
 
+from elementtree.SimpleXMLWriter import XMLWriter
+
 from lxml import etree
 from lxml.html import tostring
 
@@ -17,21 +19,17 @@ def get_news_urls():
     for p in path:
         urls.append('http://yle.fi' + p)
     urls = urls[:-1]
-    return urls[:2]
+    return urls[:5]
 
-def get_articles_dates(urls):
-    dates = []
-    for u in urls:
-        page = etree.parse(u, etree.HTMLParser(encoding='utf-8'))
-        path = page.xpath("//div[@class='block-module clearfix']/p[@class='published']//text()")
-        for p in path:
-            dates.append(p)
-    return dates[:-1]
+def get_articles_dates(u):
+    page = etree.parse(u, etree.HTMLParser(encoding='utf-8'))
+    path = page.xpath("//div[@class='block-module clearfix']/p[@class='published']//text()")
+    return path[0]
 
 def get_articles_title(url):
     page = etree.parse(url, etree.HTMLParser(encoding='utf-8'))
     path = page.xpath("//div/h1//text()")
-    return path
+    return path[0]
 
 def get_subnews_titles(url):
     page = etree.parse(url, etree.HTMLParser(encoding='utf-8'))
@@ -56,25 +54,49 @@ def get_subnews_author(url):
         res = re.sub(r'[^\w ]', '', p)
         authors.append(res)
     if len(authors) > 1:
-        return authors[1:]
+        return authors
     else:
         return authors
 
 urls = get_news_urls()
-dates = get_articles_dates(urls)
 
 for u in urls:
     title = get_articles_title(u)
     article_content = get_articles_content(u)
-    
-    print u
-    print title
-    print article_content
-    
+    authors = get_subnews_author(u)
     subnews = get_subnews_titles(u)
     subnews_content = get_subnews_content(u)
-    author = get_subnews_author(u)
+    date = get_articles_dates(u)
 
-    data = zip(subnews, subnews_content, author)
+    filename = 'nl_' + title.replace(' ','_').lower() + '.xml'
+    w = XMLWriter(filename, encoding='utf-8')
+    html = w.start("broadcast")
+
+    content = ' '.join(article_content)
+    if len(authors) > 1:
+        authorship = ','.join(set(authors))
+    else:
+        authorship = authors[0]
+
+    w.element("title", title)
+    w.element("meta", name="author", value=authorship)
+    w.element("meta", name="published", value=date)
+    w.element("meta", name="corpus", value=filename)
+    w.element("meta", name="source", value=u)
+    w.element("meta", name="generator", value="https://github.com/caio1982/Nuntii-Latini")
+    
+    w.start("headline")
+    w.element("title", title)
+    w.element("content", content)
+    w.end("headline")
+
+    data = zip(subnews, subnews_content)
     for d in data:
-        print d
+        subnews = d[0]
+        subnews_content = d[1]
+        w.start("news")
+        w.element("title", subnews)
+        w.element("content", subnews_content)
+        w.end("news")
+    
+    w.close(html)
