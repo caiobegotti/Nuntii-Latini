ab4534b2377b45cf8eeffdceb54208d493373ddf
Caio Begotti <caio1982@gmail.com>
2012-03-02 21:32:48 -0300

    now getting the news references, titles and publishing dates

--- nuntii_latini_scrapper.py
+++ nuntii_latini_scrapper.py
@@ -7,26 +7,36 @@ from lxml import etree
 from lxml.html import tostring
 
 def get_news_urls():
-    news_urls = []
+    urls = []
     url = 'http://yle.fi/radio1/tiede/nuntii_latini/'
     page = etree.parse(url, etree.HTMLParser(encoding='utf-8'))
     path = page.xpath("//div[@class='block-module clearfix']/h2/a/@href")
     for p in path:
-        news_urls.append('http://yle.fi' + p)
-    news_urls = news_urls[:-1]
-    return news_urls
+        urls.append('http://yle.fi' + p)
+    urls = urls[:-1]
+    return urls[:3]
 
-def get_news_count():
-    news = get_news_urls()
-    return len(news)
-
-def get_news_titles():
-    for url in news_urls:
-        page = etree.parse(url, etree.HTMLParser(encoding='utf-8'))
+def get_news_titles(urls):
+    titles = []
+    for u in urls:
+        page = etree.parse(u, etree.HTMLParser(encoding='utf-8'))
         path = page.xpath("//div/h1//text()")
         for p in path:
-            news_titles.append(p)
-        news_titles = news_titles[:-1]
+            titles.append(p)
+    return titles
 
-count = get_news_count()
-print 'Found %d news articles' % count
+def get_news_dates(urls):
+    dates = []
+    for u in urls:
+        page = etree.parse(u, etree.HTMLParser(encoding='utf-8'))
+        path = page.xpath("//div[@class='block-module clearfix']/p[@class='published']//text()")
+        for p in path:
+            dates.append(p)
+    return dates
+
+urls = get_news_urls()
+dates = get_news_dates(urls)
+titles = get_news_titles(urls)
+data = zip(titles, dates, urls)
+for d in data:
+    print d
