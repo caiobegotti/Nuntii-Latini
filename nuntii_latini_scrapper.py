3536c7910666eb62bd4bb74a6372d30864d12d1d
Caio Begotti <caio1982@gmail.com>
2012-03-03 09:54:03 -0300

    simplifying authors scrapping

--- nuntii_latini_scrapper.py
+++ nuntii_latini_scrapper.py
@@ -19,7 +19,7 @@ def get_news_urls():
     for p in path:
         urls.append('http://yle.fi' + p)
     urls = urls[:-1]
-    return urls[:2]
+    return urls
 
 def get_articles_dates(u):
     page = etree.parse(u, etree.HTMLParser(encoding='utf-8'))
@@ -57,23 +57,20 @@ def get_subnews_content(url):
     return path[:-8]
 
 def get_subnews_author(url):
-    authors = []
     page = etree.parse(url, etree.HTMLParser(encoding='utf-8'))
-    path = page.xpath("//p[starts-with(text(),'(')]//text()")
-    for p in path:
-        res = sub(r'[^\w ]', '', p)
-        authors.append(res)
-    if len(authors) > 1:
-        return authors
+    path = page.xpath("//p[starts-with(text(),'(')][1]//text()")
+    if path:
+        res = sub(r'[^\w ]', '', path[0])
+        return res
     else:
-        return authors
+        return 'N/A'
 
 urls = get_news_urls()
 
 for u in urls:
     title = get_articles_title(u)
     article_content = get_articles_content(u)
-    authors = get_subnews_author(u)
+    author = get_subnews_author(u)
     subnews = get_subnews_titles(u)
     subnews_content = get_subnews_content(u)
     date = get_articles_dates(u)
@@ -84,13 +81,9 @@ for u in urls:
     html = w.start("broadcast")
 
     content = ' '.join(article_content)
-    if len(authors) > 1:
-        authorship = ','.join(set(authors))
-    else:
-        authorship = authors[0]
 
     w.element("title", title)
-    w.element("meta", name="author", value=authorship)
+    w.element("meta", name="author", value=author)
     w.element("meta", name="published", value=date)
     w.element("meta", name="corpus", value=filename)
     w.element("meta", name="source", value=u)
