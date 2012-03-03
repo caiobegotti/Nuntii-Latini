04100ce537b779ad73e163e5b9c16cc56ec11d7a
Caio Begotti <caio1982@gmail.com>
2012-03-02 22:47:51 -0300

    can now scrap all the articles subnews, their content and authorship

--- nuntii_latini_scrapper.py
+++ nuntii_latini_scrapper.py
@@ -3,6 +3,8 @@
 # this shell scrapper is under public domain, go wild
 # caio begotti <caio1982@gmail.com>
 
+import re
+
 from lxml import etree
 from lxml.html import tostring
 
@@ -14,7 +16,7 @@ def get_news_urls():
     for p in path:
         urls.append('http://yle.fi' + p)
     urls = urls[:-1]
-    return urls[:3]
+    return urls[:1]
 
 def get_articles_titles(urls):
     titles = []
@@ -34,9 +36,47 @@ def get_articles_dates(urls):
             dates.append(p)
     return dates
 
+def get_subnews_titles(urls):
+    news = []
+    for u in urls:
+        page = etree.parse(u, etree.HTMLParser(encoding='utf-8'))
+        path = page.xpath("//h3//text()")
+        for p in path:
+            news.append(p)
+    return news[:-3]
+
+def get_articles_content(urls):
+    content = []
+    for u in urls:
+        page = etree.parse(u, etree.HTMLParser(encoding='utf-8'))
+        path = page.xpath("//div[@class='kuuntele-netissa']/following::p[not(preceding::h3)]//text()")
+        for p in path:
+            content.append(p)
+    return content
+
+def get_subnews_content(urls):
+    content = []
+    for u in urls:
+        page = etree.parse(u, etree.HTMLParser(encoding='utf-8'))
+        path = page.xpath("//h3/following-sibling::p//text()")
+        for p in path:
+            content.append(p)
+    return content[:-9]
+
+def get_subnews_authors(urls):
+    authors = []
+    for u in urls:
+        page = etree.parse(u, etree.HTMLParser(encoding='utf-8'))
+        path = page.xpath("//p[starts-with(text(),'(')]//text()")
+        for p in path:
+            res = re.sub(r'[^\w ]', '', p)
+            authors.append(res)
+    return authors
+
 urls = get_news_urls()
 dates = get_articles_dates(urls)
 titles = get_articles_titles(urls)
-data = zip(titles, dates, urls)
-for d in data:
-    print d
+subnews = get_subnews_titles(urls)
+article_content = get_articles_content(urls)
+subnews_content = get_subnews_content(urls)
+authors = get_subnews_authors(urls)
