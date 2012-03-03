d1481441ed2fa826a4b33987697c4e6fa068d02e
Caio Begotti <caio1982@gmail.com>
2012-03-03 02:07:34 -0300

    bunch of random uncommited stuff, but it is freaking working, at last

--- nuntii_latini_scrapper.py
+++ nuntii_latini_scrapper.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # corpora of public posts from nuntii latini, rights reserved
 # this shell scrapper is under public domain, go wild
 # caio begotti <caio1982@gmail.com>
@@ -16,16 +17,7 @@ def get_news_urls():
     for p in path:
         urls.append('http://yle.fi' + p)
     urls = urls[:-1]
-    return urls[:1]
-
-def get_articles_titles(urls):
-    titles = []
-    for u in urls:
-        page = etree.parse(u, etree.HTMLParser(encoding='utf-8'))
-        path = page.xpath("//div/h1//text()")
-        for p in path:
-            titles.append(p)
-    return titles
+    return urls[:2]
 
 def get_articles_dates(urls):
     dates = []
@@ -34,49 +26,55 @@ def get_articles_dates(urls):
         path = page.xpath("//div[@class='block-module clearfix']/p[@class='published']//text()")
         for p in path:
             dates.append(p)
-    return dates
+    return dates[:-1]
 
-def get_subnews_titles(urls):
-    news = []
-    for u in urls:
-        page = etree.parse(u, etree.HTMLParser(encoding='utf-8'))
-        path = page.xpath("//h3//text()")
-        for p in path:
-            news.append(p)
-    return news[:-3]
+def get_articles_title(url):
+    page = etree.parse(url, etree.HTMLParser(encoding='utf-8'))
+    path = page.xpath("//div/h1//text()")
+    return path
 
-def get_articles_content(urls):
-    content = []
-    for u in urls:
-        page = etree.parse(u, etree.HTMLParser(encoding='utf-8'))
-        path = page.xpath("//div[@class='kuuntele-netissa']/following::p[not(preceding::h3)]//text()")
-        for p in path:
-            content.append(p)
-    return content
+def get_subnews_titles(url):
+    page = etree.parse(url, etree.HTMLParser(encoding='utf-8'))
+    path = page.xpath("//h3//text()")
+    return path[:-3]
 
-def get_subnews_content(urls):
-    content = []
-    for u in urls:
-        page = etree.parse(u, etree.HTMLParser(encoding='utf-8'))
-        path = page.xpath("//h3/following-sibling::p//text()")
-        for p in path:
-            content.append(p)
-    return content[:-9]
+def get_articles_content(url):
+    page = etree.parse(url, etree.HTMLParser(encoding='utf-8'))
+    path = page.xpath("//div[@class='kuuntele-netissa']/following::p[not(preceding::h3) and not(starts-with(text(),'('))]//text()")
+    return path
 
-def get_subnews_authors(urls):
+def get_subnews_content(url):
+    page = etree.parse(url, etree.HTMLParser(encoding='utf-8'))
+    path = page.xpath("//h3/following-sibling::p[not(starts-with(text(),'('))]//text()")
+    return path[:-8]
+
+def get_subnews_author(url):
     authors = []
-    for u in urls:
-        page = etree.parse(u, etree.HTMLParser(encoding='utf-8'))
-        path = page.xpath("//p[starts-with(text(),'(')]//text()")
-        for p in path:
-            res = re.sub(r'[^\w ]', '', p)
-            authors.append(res)
-    return authors
+    page = etree.parse(url, etree.HTMLParser(encoding='utf-8'))
+    path = page.xpath("//p[starts-with(text(),'(')]//text()")
+    for p in path:
+        res = re.sub(r'[^\w ]', '', p)
+        authors.append(res)
+    if len(authors) > 1:
+        return authors[1:]
+    else:
+        return authors
 
 urls = get_news_urls()
 dates = get_articles_dates(urls)
-titles = get_articles_titles(urls)
-subnews = get_subnews_titles(urls)
-article_content = get_articles_content(urls)
-subnews_content = get_subnews_content(urls)
-authors = get_subnews_authors(urls)
+
+for u in urls:
+    title = get_articles_title(u)
+    article_content = get_articles_content(u)
+    
+    print u
+    print title
+    print article_content
+    
+    subnews = get_subnews_titles(u)
+    subnews_content = get_subnews_content(u)
+    author = get_subnews_author(u)
+
+    data = zip(subnews, subnews_content, author)
+    for d in data:
+        print d
